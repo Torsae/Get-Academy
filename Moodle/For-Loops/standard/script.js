430ca13ec205008eb22f1becb9c481194106bae2
Torsae <157711034+Torsae@users.noreply.github.com>
2024-03-01 16:08:59 +0100

    new task
    for loop tasks

---
+++ Moodle/For-Loops/standard/script.js
@@ -0,0 +1,25 @@
+// Javascript
+
+// Model
+const root = document.getElementById('root');
+let stopValue= 10;
+
+// View
+updateView();
+
+function updateView() {
+    root.innerHTML = /*HTML*/`
+    <div class='container'>
+            <button onclick='printNumbers()'>Print numbers</button>
+            <div id=numbers></div>
+    </div>
+`;
+}
+
+// Controller
+function printNumbers() {
+    for (let counter = 1; counter<=stopValue; counter++) {
+        document.getElementById('numbers').innerHTML += '<li>' + counter*2 + '</li>';
+    }
+
+}
---
+++ Moodle/For-Loops/with string/script.js
@@ -0,0 +1,50 @@
+// Javascript
+
+
+// Model
+const root = document.getElementById('root');
+
+// View
+updateView();
+
+function updateView() {
+    root.innerHTML = /*HTML*/`
+    <div class='container'>
+    <div>
+    <input id='txt'
+    type='text'
+    onfocusout="refocusOnInput()"
+    onchange=printLettersInReverse(this.value)>
+    <button onclick="reset('full');">Clear list</button>
+    </div>
+    <div id='letters'></div>
+    </div>
+    `;
+    refocusOnInput();
+}
+
+// Controller
+
+function printLettersInReverse(inputString) {
+    let textInput = '';
+    let reversedText = '';
+    
+    for (let index = 0; index <= inputString.length; index++) {
+        textInput = textInput + inputString.charAt(index);
+    }
+    
+    reversedText = textInput.split("").reverse().join("");
+    document.getElementById('letters').innerHTML += '<li>' + reversedText + '</li>';
+    reset('half');
+}
+
+function reset(amount) {
+    console.log("clearText:")
+    document.getElementById('txt').value = '';
+    document.getElementById('txt').focus();
+    if (amount == 'full') { updateView(); }
+}
+
+function refocusOnInput() {
+    document.getElementById('txt').focus();    
+}
