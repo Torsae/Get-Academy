a9a7ce42494982ad1edf79bf87ea2496438682f7
Torsae <157711034+Torsae@users.noreply.github.com>
2024-03-01 16:17:06 +0100

    refactoring

--- Moodle/For-Loops/standard/script.js
+++ Moodle/For-Loops/standard/script.js
@@ -2,7 +2,7 @@
 
 // Model
 const root = document.getElementById('root');
-let stopValue= 10;
+let stopValue = 10;
 
 // View
 updateView();
@@ -18,8 +18,8 @@ function updateView() {
 
 // Controller
 function printNumbers() {
-    for (let counter = 1; counter<=stopValue; counter++) {
-        document.getElementById('numbers').innerHTML += '<li>' + counter*2 + '</li>';
+    for (let counter = 1; counter <= stopValue; counter++) {
+        document.getElementById('numbers').innerHTML += '<li>' + counter * 2 + '</li>';
     }
 
 }
