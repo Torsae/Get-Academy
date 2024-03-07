998e9bd53768a5d94c1a67c69ca017643ce4b077
Torsae <157711034+Torsae@users.noreply.github.com>
2024-03-07 15:40:35 +0100

    Update script.js

--- Experimental/goblin/script.js
+++ Experimental/goblin/script.js
@@ -10,22 +10,21 @@ let totalDamage = 0;
 
 const characters = [
     {
-        goblin: {
-            accuracyRequired: 6,
-            defense: { armor: 12, fireResist: 3 }
-        },
+        name: "Goblin",
+        accuracyRequired: 6,
+        defense: { armor: 12, fireResist: 3 }
+    },
 
-        human1: {
-            name: "Terje",
-            accuracy: 8,
-            attack: { damage: 20, fire: 10 },
-        },
+    {
+        name: "Terje",
+        accuracy: 8,
+        attack: { damage: 20, fire: 10 },
+    },
 
-        human2: {
-            name: "Per",
-            accuracy: 4,
-            attack: { damage: 60, fire: 2 },
-        }
+    {
+        name: "Per",
+        accuracy: 4,
+        attack: { damage: 60, fire: 2 },
     }
 ]
 
@@ -36,17 +35,17 @@ updateView();
 function updateView() {
     root.innerHTML = /*HTML*/`
         <div>Goblin health: ${goblinHealth}</div>
-        <button onclick='attackEnemy(0)'>Try to attack</button>
+        <button onclick='attackEnemy()'>Try to attack</button>
         <div>${attackResult}</div>
     `;
 }
 
 // Controller
 
-function attackEnemy(index) {
-    unit = characters[index];
-    terje = unit.human1;
-    per = unit.human2; 
+function attackEnemy() {
+    goblin = characters[0];
+    terje = characters[1];
+    per = characters[2]; 
     playerAttack(terje);
     playerAttack(per);
     updateView();
@@ -54,7 +53,7 @@ function attackEnemy(index) {
 
 function playerAttack(unitAttacking) {
     console.log(unitAttacking.name,"is trying to attack the goblin!");
-    if (unitAttacking.accuracy >= unit.goblin.accuracyRequired) {attackHit(unitAttacking); }
+    if (unitAttacking.accuracy >= goblin.accuracyRequired) {attackHit(unitAttacking); }
     else { console.log(unitAttacking.name,"'s attack missed!"); }
 }
 
@@ -66,11 +65,11 @@ function attackHit(unitAttacking) {
 }
 
 function calculateDamage(unitAttacking) {
-    totalDamage = (unitAttacking.attack.damage - unit.goblin.defense.armor) + (unitAttacking.attack.fire - unit.goblin.defense.fireResist)
+    totalDamage = (unitAttacking.attack.damage - goblin.defense.armor) + (unitAttacking.attack.fire - goblin.defense.fireResist)
     console.log("Calculating damge taken:(",
                 unitAttacking.attack.damage,"-",
-                unit.goblin.defense.armor,") + (",
+                goblin.defense.armor,") + (",
                 unitAttacking.attack.fire,"-",
-                unit.goblin.defense.fireResist,
+                goblin.defense.fireResist,
                 ") =",totalDamage)
 }
